e9b55163b66efd254efdef022e7aa4c04a2c6aff
Rustam Karimov <rustam.karimov@rtlabs.ru>
2020-01-20 11:34:50 +0400

    Create simple spring security application

---
+++ ch03/src/main/java/com/appress/pss/springsecurity/configuration/AppInitializer.java
@@ -0,0 +1,22 @@
+package com.appress.pss.springsecurity.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{UserConfiguration.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+}
