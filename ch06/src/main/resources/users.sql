38c783d6143e5b5652df51b871f3071c9cd75057
Rustam Karimov <rkarimov.r@gmail.com>
2020-03-03 17:04:44 +0400

    Chapter 6 - example with user groups (xml context config);

--- ch06/src/main/resources/users.sql
+++ ch06/src/main/resources/users.sql
@@ -3,3 +3,10 @@ VALUES ('mnardone', 'nardone', true);
 INSERT INTO AUTHORITIES
 VALUES ('mnardone', 'ROLE_NARDONE_MEMBER');
 COMMIT;
+
+insert into GROUPS
+values (1, 'GROUP_MEMBERS_NARDONE');
+insert into GROUP_MEMBERS
+values (1, 'mnardone');
+insert into GROUP_AUTHORITIES
+values (1, 'ROLE_NARDONE_MEMBER');
